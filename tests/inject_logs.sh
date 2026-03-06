2a7aca00ab7bc54efc67f3fdc80eae497b5e863d
Sayan Hazra <sayanhazra625@gmail.com>
2026-03-06 11:25:06 +0530

    added test files

---
+++ tests/inject_logs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Injecting logs..."
+
+docker exec service-a-test sh -c "echo TEST_LOG_A"
+docker exec service-b-test sh -c "echo TEST_LOG_B"
+docker exec service-c-test sh -c "echo TEST_LOG_C"
+
+sleep 5
---
+++ tests/verify_pipeline.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Checking collector logs..."
+
+docker logs central-collector > collector_output.log
+
+grep TEST_LOG_A collector_output.log
+grep TEST_LOG_B collector_output.log
+grep TEST_LOG_C collector_output.log
+
+if [ $? -eq 0 ]; then
+  echo "Pipeline test successful"
+else
+  echo "Pipeline test failed"
+  exit 1
+fi
